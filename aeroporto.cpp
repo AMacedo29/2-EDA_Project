27959f676db3995afe37c86653727aebafccf9f9
AMacedo29 <andrejesus.sa@hotmail.com>
2024-04-29 12:56:38 +0100

    O menu agr tem opções (falta por as funções para dentro so temos os prints)

--- aeroporto.cpp
+++ aeroporto.cpp
@@ -39,5 +39,35 @@ void aeroportoMenu(){
     cout << "Destino: " <<endl;
     cout << "Passageiros: " <<endl;
 
+    ///////////////////////////////////////////////////////////
 
+    char escolha;
+    bool sair = false;
+
+    do{
+        cout << "Digite a sua escolha (e)mergencias (o)pcoes (g)ravar (s)air" << endl;
+        cin >> escolha;
+
+        //Convertendo a escolha para minusculas
+        escolha = tolower(escolha);
+
+    switch(escolha) {
+        case 'e':
+            cout <<" ainda n tem nada " << endl;
+            break;
+        case 'o':
+            cout << "ainda n tem nada " << endl;
+            break;
+        case 'g':
+            cout << "ainda n tem nada " << endl;
+            break;
+        case 's':
+            cout << "Escolheu a opcao Sair. Adeus!" << endl;
+            sair = true;
+            return;
+        default:
+            cout << "Opção inválida!" << endl;
+            break;
+        }
+    }while (!sair);
 };
