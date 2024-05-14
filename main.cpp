39dd0a66a880bfb9dbb679b7616156e1ca13d862
AMacedo29 <andrejesus.sa@hotmail.com>
2024-05-14 18:11:25 +0100

    TODO:

--- main.cpp
+++ main.cpp
@@ -14,3 +14,11 @@ int main(){
 
     return 0;
 }
+
+//TODO: a lista pista so esta a receber 6 avioes (deveria ser 7)
+//TODO:"Na gravação faz gravação em 3 ficheiros separados e vez de ser um ficheiro único"
+//TODO:mudar o destino dos avioes em aprox para Aeroporto EDA e a orgiem dos que estão na pista e na partida para Aeroporto EDA
+//TODO:3.1.3 faço a minima como é que se faz isso
+//TODO:quando um aviao chega a pista temos que lhe dar outro nomeVoo
+//TODO:falta o 3.3 / 3.4 / 3.5 / 3.7 / 5
+//TODO:3.6 - quando a lista de aprox ultrapassar os 10 passageiros ele trata de 2 avioes em simultaneo ate voltar ao normal
