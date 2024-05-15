f427198459c01feabd0ad2729d7389478b02e5b6
AMacedo29 <andrejesus.sa@hotmail.com>
2024-05-15 15:32:44 +0100

    update do TODO: (era para ter dado junto com o outro commit)

--- main.cpp
+++ main.cpp
@@ -16,7 +16,6 @@ int main(){
 }
 
 //TODO:"Na gravação faz gravação em 3 ficheiros separados e vez de ser um ficheiro único"
-//TODO:mudar o destino dos avioes em aprox para Aeroporto EDA e a orgiem dos que estão na pista e na partida para Aeroporto EDA
 //TODO:3.1.3 faço a minima como é que se faz isso
 //TODO:quando um aviao chega a pista temos que lhe dar outro nomeVoo
 //TODO:falta o 3.3 / 3.4 / 3.5 / 3.7 / 5
